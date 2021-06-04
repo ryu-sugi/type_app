396ac2220b7911cf8b29d56370d0342eed4ae091
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-06-04 15:37:11 +0900

    javascript 骨組み作成

---
+++ app/javascript.js
@@ -0,0 +1,16 @@
+const wrap = document.getElementById('wrap');
+const start = document.getElementById('start');
+
+const textLists = [];
+
+const createText = () => {}; 
+
+const keyDown = e => {};
+
+const rankCheck = rank => {};
+
+const gameOver = id => {};
+
+const timer = () => {};
+
+start.addEventListener('click', () => {});
