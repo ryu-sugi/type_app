8dfa23529419b35cbab52d373c7afa1a304747b1
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-06-04 15:53:22 +0900

    文字列表示

--- app/javascript.js
+++ app/javascript.js
@@ -1,9 +1,18 @@
 const wrap = document.getElementById('wrap');
 const start = document.getElementById('start');
 
-const textLists = [];
+const textLists = [
+ 'Hello World',
+ 'This is my App',
+ 'How are you?'
+];
 
-const createText = () => {}; 
+const createText = () => {
+ const p = document.getElementById('text');
+ p.textContent = textLists[0];
+}; 
+
+createText();
 
 const keyDown = e => {};
 
